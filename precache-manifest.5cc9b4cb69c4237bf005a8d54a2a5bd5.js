e57ad63fdb25ef32b4c78698d5bb5b51489312fa
Mashbean <mashbean@gmail.com>
2026-05-20 12:57:51 +0800

    Updates

---
+++ precache-manifest.5cc9b4cb69c4237bf005a8d54a2a5bd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c63731ee723d1305467d6a73664265",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c7e00031a884fe84379c",
+    "url": "./static/css/main.25b7d723.chunk.css"
+  },
+  {
+    "revision": "46ee1f7bb43d388a5893",
+    "url": "./static/js/2.d389cce9.chunk.js"
+  },
+  {
+    "revision": "c7e00031a884fe84379c",
+    "url": "./static/js/main.5e655893.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
